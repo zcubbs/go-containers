1a38b04c1d828d801cfc0b5aad8f1537080b37cf
zakaria <52750533+zcubbs@users.noreply.github.com>
2023-08-04 03:38:09 +0200

    :white_check_mark: update test

--- run_container_test.go
+++ run_container_test.go
@@ -1,10 +1,15 @@
 package gocontainers
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestRunContainerOnHost(t *testing.T) {
 	err := RunContainerOnHost("docker.io/library/alpine", []string{"echo", "hello world"})
 	if err != nil {
 		t.Error(err)
 	}
+
+	time.Sleep(5 * time.Second)
 }
